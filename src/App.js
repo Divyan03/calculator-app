9b854af8981e234d5858cd72c35a79a34c504b8c
Divyan03 <111556193+Divyan03@users.noreply.github.com>
2023-01-05 22:05:20 -0500

    calculator updates
    -Everything working
    -Some edge cases can be changed

--- src/App.js
+++ src/App.js
@@ -1,31 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+  const ops = ["/","*", "-", "+", "."];
+
+  const updateCalc = value => {
+    if (ops.includes(value) && calc == '' || ops.includes(value) && ops.includes(calc.slice(-1))){
+      return;
+    }
+    if (!ops.includes(value)){
+      setResult(eval(calc + value).toString());
+    }
+    setCalc(calc + value);
+  }
+
+  const clear = () => {
+    setCalc("");
+  }
+
+  const backspace = () => {
+    setCalc(calc.slice(0,-1));
+  }
+
+  const evaluate = () =>{
+    setCalc(eval(calc).toString());
+  }
+  
   return (
     <div className="Calculator-grid">
       <div className="Output">
-        <div className="lastOutput">3123</div>
-        <div className="currentOutput">31223</div>
+        <div className="lastOutput">{result || ""}</div>
+        <div className="currentOutput">{calc || 0}</div>
       </div>
-      <button className="span-two">AC</button>
-      <button>DEL</button>
-      <button>÷</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>x</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>-</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>+</button>
-      <button>0</button>
-      <button>.</button>
-      <button className="span-two">=</button>
+      <button className="span-two" onClick={clear}>AC</button>
+      <button className="operations" onClick={backspace}>DEL</button>
+      <button className="operations" onClick={() => updateCalc('/')}>÷</button>
+      <button className="keypad" onClick={() => updateCalc('7')}>7</button>
+      <button className="keypad" onClick={() => updateCalc('8')}>8</button>
+      <button className="keypad" onClick={() => updateCalc('9')}>9</button>
+      <button className="operations" onClick={() => updateCalc('*')}>x</button>
+      <button className="keypad" onClick={() => updateCalc('4')}>4</button>
+      <button className="keypad" onClick={() => updateCalc('5')}>5</button>
+      <button className="keypad" onClick={() => updateCalc('6')}>6</button>
+      <button className="operations" onClick={() => updateCalc('-')}>-</button>
+      <button className="keypad" onClick={() => updateCalc('1')}>1</button>
+      <button className="keypad" onClick={() => updateCalc('2')}>2</button>
+      <button className="keypad" onClick={() => updateCalc('3')}>3</button>
+      <button className="operations" onClick={() => updateCalc('+')}>+</button>
+      <button className="keypad" onClick={() => updateCalc('0')}>0</button>
+      <button className="keypad" onClick={() => updateCalc('.')}>.</button>
+      <button className="span-two" onClick={evaluate}>=</button>
     </div>
   );
 }
