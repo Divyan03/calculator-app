453fc35a3486ef3647f84c240d8f05bde92f4d08
Divyan03 <111556193+Divyan03@users.noreply.github.com>
2023-01-05 19:54:04 -0500

    Base calculator

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Calculator-grid">
+      <div className="Output">
+        <div className="lastOutput">3123</div>
+        <div className="currentOutput">31223</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>x</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>-</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>+</button>
+      <button>0</button>
+      <button>.</button>
+      <button className="span-two">=</button>
+    </div>
+  );
+}
+
+export default App;
